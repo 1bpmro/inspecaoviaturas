ebe41a445a327f2d7bf6a84899802230daa4cdf2
1bpmro <ti1bpmro@gmail.com>
2026-02-24 09:27:24 -0400

    Update gasClient.js

--- src/api/gasClient.js
+++ src/api/gasClient.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const GAS_URL = 'https://script.google.com/macros/s/AKfycbxqssl_yJzN1ugOIAve3D2_xkvJ4507AXmwsuBTAk3-jiuvYxk7AN-2dZfsr1D6cUyJ9A/exec';
 
 export const gasApi = {
-  // Função genérica para simplificar as chamadas
   post: async (action, payload = {}) => {
     try {
       const response = await axios.post(GAS_URL, {
@@ -17,18 +16,13 @@ export const gasApi = {
   },
 
   login: (re, senha) => gasApi.post('login', { re, senha }),
-  
   checkProfile: (re) => gasApi.post('checkProfile', { re }),
-  
   saveVistoria: (dados) => gasApi.post('saveVistoria', dados),
-  
   getViaturas: (apenasEmServico) => gasApi.post('getViaturas', { apenasEmServico }),
+  buscarMilitar: (re) => gasApi.post('buscarMilitar', { re }),
   
-  buscarMilitar: (re) => gasApi.post('buscarMilitar', { re }) // Adicionado aqui!
-
+  // Rotas do Garageiro
   getVistoriasPendentes: () => gasApi.post('getVistoriasPendentes'),
-  
   confirmarVistoriaGarageiro: (dados) => gasApi.post('confirmarVistoriaGarageiro', dados),
-  
-  alterarStatusViatura: (prefixo, novoStatus) => gasApi.post('alterarStatusViatura', { prefixo, novoStatus }),
+  alterarStatusViatura: (prefixo, novoStatus) => gasApi.post('alterarStatusViatura', { prefixo, novoStatus })
 };
