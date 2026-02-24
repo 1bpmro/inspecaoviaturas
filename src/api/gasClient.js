4193c2bf8c3d061677ef6aa39d16674fd6088bcd
1bpmro <ti1bpmro@gmail.com>
2026-02-24 13:07:07 -0400

    Update gasClient.js

--- src/api/gasClient.js
+++ src/api/gasClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzjIcT67hADQRmCBnS2V9-DNSaBCkWY4pOPITpqmVplo-9aIFicRbYO1iWkI_aesI3dMQ/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbx362OzXA5j4F168SPQB--JS8TeUU_FVel_07bLwKY7tElzeHaBa1pYpXpijUltPHM5kA/exec';
 
 export const gasApi = {
   post: async (action, payload = {}) => {
