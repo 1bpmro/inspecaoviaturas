f7f8507466cf970c76be4944bdbf2712f956ef7f
1bpmro <ti1bpmro@gmail.com>
2026-03-10 12:05:31 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,23 @@
-const CACHE_NAME = 'v1-derso-1bpm';
-const urlsToCache = ['/'];
+const CACHE_NAME = 'inspecao-viaturas-1bpm';
+
+// Ajuste os caminhos para incluir o nome do repositório
+const urlsToCache = [
+  '/inspecaoviaturas/',
+  '/inspecaoviaturas/index.html',
+  '/inspecaoviaturas/manifest.json',
+  '/inspecaoviaturas/icon-512.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      // Usamos cache.addAll apenas para o que for ESSENCIAL
+      // Se um arquivo falhar, ele não trava o resto
+      return Promise.all(
+        urlsToCache.map(url => {
+          return cache.add(url).catch(err => console.warn('Falha ao cachear:', url));
+        })
+      );
     })
   );
 });
