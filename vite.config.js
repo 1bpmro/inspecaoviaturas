e03c89ab4f876f8a3b2c45851b34c3e6253d9e17
1bpmro <ti1bpmro@gmail.com>
2026-03-11 12:35:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,34 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+
   plugins: [react()],
-  // A base deve ser EXATAMENTE o nome do seu repositório entre barras
-  base: '/inspecaoviaturas/', 
+
+  // nome do repositório no GitHub
+  base: '/inspecaoviaturas/',
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  build: {
+
+    // garante pasta de assets estável
+    assetsDir: "assets",
+
+    // melhora compatibilidade com celulares
+    target: "es2018",
+
+    // limpa build antigo
+    emptyOutDir: true
+
+  },
+
+  server: {
+    port: 5173,
+    open: true
+  }
+
 })
