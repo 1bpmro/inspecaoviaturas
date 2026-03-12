a4d1d65982bfab06fb2e3126d60dab03effea23b
1bpmro <ti1bpmro@gmail.com>
2026-03-12 09:13:42 -0400

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, serverTimestamp, collection, addDoc, updateDoc } from "firebase/firestore";
-import { getStorage, ref, uploadString, getDownloadURL } from "firebase/storage";
+import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDuFFlCVg8pAF6h2bv3XMTamOMaQMe0zx4",
@@ -12,10 +11,8 @@ const firebaseConfig = {
   measurementId: "G-2VNSCZN9JP"
 };
 
-// Inicialização
 const app = initializeApp(firebaseConfig);
 
-// Instâncias dos serviços
+// Exportamos apenas o necessário do Firestore
 export const db = getFirestore(app);
-export const storage = getStorage(app);
-export { serverTimestamp, collection, addDoc, updateDoc, ref, uploadString, getDownloadURL };
+export { collection, addDoc, serverTimestamp };
